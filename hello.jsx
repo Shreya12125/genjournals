8d24e47489115c7ab360374a90c8fa1feb700102
Shreya Sriram <164351677+Shreya12125@users.noreply.github.com>
2025-08-08 15:02:34 +0530

    Add files via upload

---
+++ hello.jsx
@@ -0,0 +1,27 @@
+{/* <View classname="bg-primary">
+            <View>
+              <Image source={images.logo}
+                        resizeMode='contain'
+                        className='w-[400px] h-[200px]'
+
+                  />
+            </View>
+            <View>
+              <Card
+                className="w-[95vw] bg-primary py-4 my-2 border border-gray-700 rounded-xl"  
+                         
+              >
+                <Text className="text-white text-lg mb-1"> Welcome Anand  </Text>
+                <Card.Actions>
+                    <IconButton
+                          
+                          iconColor={'#737373'}
+                          size={20}
+                          onPress={() => console.log('icon is Pressed')}
+                    />
+                </Card.Actions> 
+              </Card>
+            </View>
+            {userName && (<Text>Hello {userName} </Text>)}
+
+          </View> */}
